682d3609895ba5fd21dd21bedaee25ab0de7df14
BenAwuor2 <99531759+BenAwuor2@users.noreply.github.com>
2024-01-27 13:23:41 +0300

    log_parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,48 +1,38 @@
 #!/usr/bin/python3
-"""
-module contains a script that reads stdin line by line and computes metrics
-"""
+
 import sys
+from collections import defaultdict
 
+def print_statistics(total_size, status_counts):
+    print(f"Total file size: {total_size}")
+    for status_code in sorted(status_counts.keys()):
+        print(f"{status_code}: {status_counts[status_code]}")
 
-status_codes = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0
-}
+def main():
+    total_size = 0
+    status_counts = defaultdict(int)
+    line_count = 0
 
-file_size = 0
-
-
-def print_metrics():
-    """prints of the logs"""
-    print("File size: {}".format(file_size))
-    for status in sorted(status_codes.keys()):
-        if status_codes[status]:
-            print("{}: {}".format(status, status_codes[status]))
-
-
-if __name__ == "__main__":
-    count = 0
     try:
         for line in sys.stdin:
-            try:
-                elems = line.split()
-                file_size += int(elems[-1])
-                if elems[-2] in status_codes:
-                    status_codes[elems[-2]] += 1
-            except Exception:
-                pass
-            if count == 9:
-                print_metrics()
-                count = -1
-            count += 1
+            line = line.strip()
+            parts = line.split()
+            if len(parts) != 7:
+                continue
+
+            ip_address, date, request, status_code, file_size = parts
+            if status_code.isdigit():
+                status_counts[status_code] += 1
+            total_size += int(file_size)
+
+            line_count += 1
+            if line_count == 10:
+                print_statistics(total_size, status_counts)
+                line_count = 0
+
     except KeyboardInterrupt:
-        print_metrics()
-        raise
-    print_metrics()
+        print_statistics(total_size, status_counts)
+        sys.exit(0)
+
+if __name__ == "__main__":
+    main()
